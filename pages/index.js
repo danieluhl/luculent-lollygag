7e78a8f70593dd986cf72fc9f6b5e6fe991acb5b
Dan Uhl <duhl@wayfair.com>
2021-04-27 19:39:58 -0400

    remove first-post

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
 
+// this runs server side and is for geting the data for this page
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
