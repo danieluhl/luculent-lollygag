a0e8dee6b7681b640fd99b23441531c329131ed8
Dan Uhl <duhl@wayfair.com>
2021-04-27 19:32:12 -0400

    adds all the things

---
+++ pages/posts/[id].js
@@ -0,0 +1,43 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+
+// runs only server side at build time to get all possible paths that will
+//  render using this component
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// runs only server side at build time and gets the props needed for
+//  one instance of this component
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+  purge: [
+    // Use *.tsx if using TypeScript
+    "./pages/**/*.js",
+    "./components/**/*.js",
+  ],
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  // ...
+};
