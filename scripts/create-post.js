4b0576477342f3f1e0124a6c3e0504d7b1d16615
Dan Uhl <duhl@wayfair.com>
2021-04-29 08:39:33 -0400

    fix styles

--- scripts/create-post.js
+++ scripts/create-post.js
@@ -1,12 +1,16 @@
-const fs = require("fs");
-const argv = require("minimist")(process.argv.slice(2));
+const fs = require('fs');
+const argv = require('minimist')(process.argv.slice(2));
+
+// give one or more titles and this will scaffold out files with title and date
+//  example: node create-post.js "my new blog post"
+//   creates 2021-04-29-my-new-blog-post.md with initial content
 
 const titles = argv._;
 
 function toTitleCase(str) {
   return str.replace(
     /\w\S*/g,
-    (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
   );
 }
 
@@ -16,9 +20,9 @@ const date = `${today.getFullYear()}-${`${today.getMonth() + 1}`.padStart(
   0
 )}-${`${today.getDate() + 1}`.padStart(2, 0)}`;
 
-titles.forEach((title) => {
+titles.forEach(title => {
   const titleCaseTitle = toTitleCase(title);
-  const filename = `${date}-${title.split(" ").join("-").toLowerCase()}`;
+  const filename = `${date}-${title.split(' ').join('-').toLowerCase()}`;
 
   fs.appendFile(
     `./posts/${filename}.md`,
@@ -30,7 +34,7 @@ date: '${date}'
 # ${titleCaseTitle}
 
 `,
-    (err) => {
+    err => {
       if (err) throw err;
       console.log(`${filename}`);
     }
