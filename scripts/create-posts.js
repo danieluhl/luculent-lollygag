f6a4c8398886f02ba12d252adf621416fab63c8d
Dan Uhl <duhl@wayfair.com>
2021-09-09 09:27:42 -0400

    adds a bunch of bunk posts

--- scripts/create-posts.js
+++ scripts/create-posts.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
+const cp = require('child_process');
 const argv = require('minimist')(process.argv.slice(2));
 const createPosts = require('./create-posts-fn');
 
-createPosts(argv._);
+const filesCreated = createPosts(argv._);
+
+// open files after created
+filesCreated.forEach(file => {
+  cp.exec(`code ${file}`);
+})
